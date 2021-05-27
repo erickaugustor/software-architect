2bec7b20630e6164d16b653f644da5b87e34d92b
Erick - <erick.augustoramos@gmail.com>
2021-05-26 23:52:54 -0300

    add changes to the graphql

--- graphql-example/example-js/index.js
+++ graphql-example/example-js/index.js
@@ -4,10 +4,10 @@ const resolvers = require('./resolvers')
 
 const schemaPath = './schema/index.graphql'
 const server = new ApolloServer({
-  typeDefs: importSchema(schemaPath),
-  resolvers
+    typeDefs: importSchema(schemaPath),
+    resolvers
 })
 
 server.listen().then(({ url }) => {
-  console.log(`Executando em ${url}`)
+    console.log(`Executando em ${url}`)
 })
